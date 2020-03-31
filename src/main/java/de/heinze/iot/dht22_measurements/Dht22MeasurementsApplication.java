40efa8710efe18ed384ac598f3e84fb7893d0f0a
Christian Heinz <kontakt@christian-heinz.eu>
2020-03-31 08:51:23 +0200

    Initial project setup

---
+++ src/main/java/de/heinze/iot/dht22_measurements/Dht22MeasurementsApplication.java
@@ -0,0 +1,13 @@
+package de.heinze.iot.dht22_measurements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dht22MeasurementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dht22MeasurementsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/heinze/iot/dht22_measurements/Dht22MeasurementsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.heinze.iot.dht22_measurements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dht22MeasurementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
